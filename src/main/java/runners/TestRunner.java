f8e17c08e96fb2eb2dd2a87bc645583fe9c5c6a7
lamhot <lamhotjmsiagian94@gmail.com>
2024-07-04 11:50:14 -0500

    start

---
+++ src/main/java/runners/TestRunner.java
@@ -0,0 +1,15 @@
+package runners;
+
+
+import org.junit.runner.RunWith;
+
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "steps"
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+}
